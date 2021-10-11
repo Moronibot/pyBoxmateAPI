891f5eac1643917ad90b27487726fe023ba7590f
Euan <Euanmeston@gmail.com>
2021-10-11 21:14:39 +0100

    WIP

--- pyboxmate/api_calls.py
+++ pyboxmate/api_calls.py
@@ -12,7 +12,6 @@ class BoxmateAPI:
     def api_call(self, endpoint: str) -> dict:
         """ Standard API call with correct token payload """
         api_call = self.api_root_page + endpoint
-        print(api_call)
         r = self.api_session.post(api_call, data={'member_token': self.api_token})
         return r
 
@@ -27,15 +26,57 @@ class BoxmateAPI:
         r = self.api_call(api_endpoint)
         return r
 
+    def lookup_member_month(self, user_id: int):
+        api_endpoint = f"/member/insights/this%20month/{user_id}"
+        r = self.api_call(api_endpoint)
+        print(len(r.json()))
+
     def exercise_scores(self, exercise_id: int) -> dict:
         api_endpoint = f"/exercise/{exercise_id}/scores"
         r = self.api_call(api_endpoint)
         return r
 
+    def box_info(self):
+        api_endpoint = "/member/home/main"
+        r = self.api_call(api_endpoint)
+        return r
+
+    def check_notifications(self):
+        api_endpoint = "/member/notifications"
+        r = self.api_call(api_endpoint)
+        return r
+
+    # Class handling
+    def get_timetable(self, year_month_day: str):
+        api_endpoint = f"/teamup/customer/events/{year_month_day}"
+        r = self.api_call(api_endpoint)
+        return r
+
+    def check_class_eligible(self, event_id: int):
+        api_endpoint = f"/teamup/customer/events/{event_id}/eligibility"
+        r = self.api_call(api_endpoint)
+        return r
+
+    def book_class(self, event_id: int):
+        api_endpoint = f"/teamup/customer/events/{event_id}/register"
+        r = self.api_call(api_endpoint)
+        return r
+
+    def cancel_class(self, event_id: int):
+        api_endpoint = f"/teamup/customer/events/{event_id}/cancel"
+        r = self.api_call(api_endpoint)
+        return r
+
+    def recent_attendances(self):
+        api_endpoint = "/teamup/customer/attendances"
+        r = self.api_call(api_endpoint)
+        return r
 
 if __name__ == '__main__':
     with open("token_config.json", 'r') as token_file:
         token_json = json.load(token_file)
         API_TOKEN = token_json['token']
     tester = BoxmateAPI(API_TOKEN)
-    tester.exercise_scores(217)
+    benedict = 40511
+    me = 40535
+    tester.lookup_member_month(me)
